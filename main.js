eeb6cb4dda870958b733ea329f9f0b24ba73eb37
Agustina Ramirez <xagusramirezx@gmail.com>
2023-09-29 17:10:48 -0300

    Correciones

--- main.js
+++ main.js
@@ -1,113 +1,90 @@
-
-let alto= parseFloat(prompt('Por favor, ingrese el alto del diseño en centimetros: '));
-let ancho= parseFloat(prompt('Por favor, ingrese el ancho del diseño en centimetros: '));
-let unidadesImprimir= parseFloat(prompt('Por favor, ingrese la cantidad de unidades que necesite imprimir: '));
-
-
-const anchoHojaA4 = 21; 
-const altoHojaA4 = 29;
-const anchoHojaA3 = 29; 
-const altoHojaA3 = 42; 
-const hoja = verificarTamano(alto,ancho,anchoHojaA4,altoHojaA4,anchoHojaA3,altoHojaA3);
-
-
-let anchoHoja= 0;
-let altoHoja= 0;
-let unidadesPorHoja= 0;
-let costoPorCopia=0;
-let contadorHojas = 0;
-let hojasImpresion=0;
-let costoTotal= 0;
-
-
-
-
-
- switch (hoja) {
-    case 1:
-        anchoHoja=anchoHojaA4;
-        altoHoja=altoHojaA4;
-        unidadesPorHoja=calcularUnidadesPorHoja(alto,ancho,anchoHoja,altoHoja);
-        costoPorCopia=150;
-        hojasImpresion = calcularHojas(unidadesPorHoja, unidadesImprimir);
-        costoTotal= calculoCosto(hojasImpresion,costoPorCopia);
-        alert ('Su pedido se imprimira en tamaño A4.');
-        break;
-    case 2:
-        anchoHoja=anchoHojaA3;
-        altoHoja=altoHojaA3;
-        unidadesPorHoja=calcularUnidadesPorHoja(alto,ancho,anchoHoja,altoHoja);
-        costoPorCopia=250;
-        hojasImpresion = calcularHojas(unidadesPorHoja, unidadesImprimir);
-        costoTotal= calculoCosto(hojasImpresion,costoPorCopia);
-        alert ('Su pedido se imprimira en tamaño A3.');
-        break;
-
-    case 3:
-    default:
-        alert ('No es posible imprimir en el tamaño que ingreso.');
-        break;
+// Función para obtener la entrada del usuario
+function obtenerEntradaUsuario(promptText) {
+    return parseInt(prompt(promptText));
 }
 
-function verificarTamano(alto,ancho,anchoHojaA4,altoHojaA4,anchoHojaA3,altoHojaA3) {
-
-    if (ancho < anchoHojaA4 && alto < altoHojaA4) {
-        return 1;
-    }  else if ( ancho < anchoHojaA3 && alto < altoHojaA3){
-        return 2;
-    } else {
-        return 3;
-    }
+// Función para mostrar opciones de papel según el tamaño de hoja
+function mostrarOpcionesPapel(opciones, tamanoHoja) {
+    console.log("Opciones de hojas disponibles para el tamaño seleccionado:");
+    opciones[tamanoHoja].forEach((opcion, index) => {
+        console.log(`${index + 1}. ${opcion}`);
+    });
 }
 
-function calcularUnidadesPorHoja(alto,ancho,anchoHoja,altoHoja) {
-    let contadorAncho=0;
-    let contadorAlto=0;
+// Función para calcular el costo de impresión
+function calcularCostoImpresion() {
+    // Solicitar datos al usuario
+    const color = obtenerEntradaUsuario("Seleccione 1 para Impresión en color o 2 para Impresión en blanco y negro:");
+    const dobleCara = obtenerEntradaUsuario("Seleccione 1 para Impresión a una sola cara o 2 para Impresión a doble cara:");
+    const tamanoHoja = obtenerEntradaUsuario("Seleccione 1 para A4, 2 para A3 o 3 para SA3:");
 
-    for (let i = ancho; i <= anchoHoja; i += ancho ) {
-        contadorAncho+=1;
-    }
+    // Definir opciones de hojas disponibles según el tamaño de hoja seleccionado
+    const opcionesHoja = {
+        1: ["70gr", "80gr", "90gr", "Ilustración 150", "Adhesivo"],
+        2: ["80gr", "90gr", "Ilustración 120", "Ilustración 150", "Ilustración 300", "Adhesivo"],
+        3: ["90gr", "Ilustración 120", "Ilustración 150", "Ilustración 300", "Adhesivo"]
+    };
     
-    for (let i = alto; i <= altoHoja; i += alto ) {
-        contadorAlto+=1;
+    // Mostrar opciones de hojas disponibles según el tamaño de hoja seleccionado
+    mostrarOpcionesPapel(opcionesHoja, tamanoHoja);
+
+    const tipoHoja = obtenerEntradaUsuario("Seleccione el número correspondiente al tipo de hoja (VER OPCIONES EN CONSOLA):");
+    const cantidadHojas = obtenerEntradaUsuario("Ingrese la cantidad de hojas a imprimir:");
+    const cortarCruz = obtenerEntradaUsuario("¿Desea cortar las hojas en forma de cruz? Ingrese 1 para sí o 2 para no:");
+
+    // Definir costos de impresión y hojas
+    const costosColor = { 1: 20, 2: 35, 3: 40 };
+    const costosBlancoNegro = { 1: 10, 2: 15, 3: 20 };
+
+    // Calcular costo base según color de impresión
+    let costoBase = color === 1 ? costosColor[tamanoHoja] : costosBlancoNegro[tamanoHoja];
+
+    // Si es impresión a doble cara, duplicar el costo
+    if (dobleCara === 2) {
+        costoBase *= 2;
     }
 
- 
-    return contadorAncho * contadorAlto ;
-}
+    // Obtener el tipo de hoja seleccionado
+    const tipoHojaSeleccionado = opcionesHoja[tamanoHoja][tipoHoja - 1];
 
-function calcularHojas(unidadesPorHoja,unidadesImprimir) {
-    let contadorHojas=0;
+    // Definir costos de hojas por tamaño
+    const costosHoja = {
+        1: [10, 20, 30, 150, 100],
+        2: [40, 50, 150, 250, 350, 200],
+        3: [60, 160, 350, 360, 230]
+    };
 
-    for (let i = unidadesPorHoja; i <= unidadesImprimir; i += unidadesPorHoja ) {
-        contadorHojas+=1;
+    // Obtener el costo de la hoja seleccionada
+    const costoHojaSeleccionada = costosHoja[tamanoHoja][tipoHoja - 1];
+
+    // Calcular costo total de impresión
+    let costoTotal = costoBase * cantidadHojas + costoHojaSeleccionada;
+
+    // Si se desea cortar en forma de cruz, agregar $500
+    if (cortarCruz === 1) {
+        costoTotal += 500;
     }
-    return contadorHojas;
+
+    // Mostrar resumen en un prompt
+    const resumenCotizacion = `Resumen de la cotización:
+Color de impresión: ${color === 1 ? 'Color' : 'Blanco y Negro'}
+Tipo de impresión: ${dobleCara === 1 ? 'Una cara' : 'Doble cara'}
+Tamaño de hoja: ${tamanoHoja === 1 ? 'A4' : tamanoHoja === 2 ? 'A3' : 'SA3'}
+Tipo de hoja: ${tipoHojaSeleccionado}
+Cantidad de hojas: ${cantidadHojas}
+Cortar en forma de cruz: ${cortarCruz === 1 ? 'Sí' : 'No'}
+Costo total: $${costoTotal}`;
+    alert(resumenCotizacion);
+    console.log(resumenCotizacion);
+
+    // Mostrar costos detallados en la consola
+    console.log("Costos detallados:");
+    console.log(`Costo base de impresión: $${costoBase}`);
+    console.log(`Costo de hoja seleccionada: $${costoHojaSeleccionada}`);
+    if (cortarCruz === 1) {
+        console.log("Costo por corte en forma de cruz: $500");
+    }
 }
 
-function calculoCosto(hojasImpresion,costoPorCopia){
-    return hojasImpresion*costoPorCopia;
-}
- 
-
-
-
-
- // mostrar los resultados
- alert("Cantidad de unidades por hoja: " + unidadesPorHoja +
-       "\nCantidad de hojas necesarias: " + hojasImpresion +
-       "\nCosto total: $" + costoTotal.toFixed(2)); 
-       // Redondear 2 decimales
-
-
-// mostrar todos los valores funcionando por consola
-console.log('Ancho del diseño = '+ ancho);
-console.log('Alto del diseño = '+ alto);
-console.log('Cantidad de unidades = '+ unidadesImprimir);
-console.log('Cantidad unidades x hoja ='+ unidadesPorHoja);
-console.log('Cantidad Hojas necesarias ='+ hojasImpresion);
-console.log('Costo total ='+ costoTotal);
-
-// console.log('hoja ='+ hoja); //entrada switch
-
-
+// Calcular el costo de impresión
+calcularCostoImpresion();
